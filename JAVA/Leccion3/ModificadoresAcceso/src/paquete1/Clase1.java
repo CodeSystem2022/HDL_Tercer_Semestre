d4fd6ae8dae7a05360f5b01982acb1f4487b851a
SantiagoCascón <112910326+SantiagoCascon@users.noreply.github.com>
2023-04-27 23:27:18 -0300

    Clase 03 Java
    Modificadores de Acceso: Public

---
+++ JAVA/Leccion3/ModificadoresAcceso/src/paquete1/Clase1.java
@@ -0,0 +1,16 @@
+package paquete1;
+
+public class Clase1 {
+    // Atributo
+    public String atributoPublic = "Valor Atributo Publico";
+    
+    // Constructor
+    public Clase1(){
+        System.out.println("Constructo publico");
+    }
+    
+    // Metodos
+    public void metodoPublico(){
+        System.out.println("Metodo Publico");
+    }
+}
