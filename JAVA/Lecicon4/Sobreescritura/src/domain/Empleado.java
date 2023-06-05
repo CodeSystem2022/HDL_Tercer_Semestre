f0456bd955941c48ffd4207448ee870701366a52
SantiagoCascón <112910326+SantiagoCascon@users.noreply.github.com>
2023-06-05 20:24:33 -0300

    Clase 04 Java
    Sobreescritura. Creamos clase padre

---
+++ JAVA/Lecicon4/Sobreescritura/src/domain/Empleado.java
@@ -0,0 +1,37 @@
+
+package domain;
+
+
+public class Empleado {
+    protected String nombre;
+    protected double sueldo;
+    
+    /*Creamos un constructor*/
+    public Empleado(String nombre, double sueldo){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        
+    }
+    /*Creamos un metodo para la sobreescritura*/
+    public String obtenerDetalles(){
+        return "Nombre: " + nombre + ", Sueldo: " + sueldo;
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+    
+}
