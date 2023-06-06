25bdbaac091039e52f2f053c9a6ee536d8859808
SantiagoCascón <112910326+SantiagoCascon@users.noreply.github.com>
2023-06-05 21:08:56 -0300

    Clase 04 JAVA
    Polimorfismo parte 1 y 2

--- JAVA/Lecicon4/Sobreescritura/src/test/TestSobreescritura.java
+++ JAVA/Lecicon4/Sobreescritura/src/test/TestSobreescritura.java
@@ -1,13 +1,22 @@
 /* Archivo de prueba*/
 package test;
 
-import domain.Gerente;
+import domain.*;
 
 public class TestSobreescritura {
     public static void main(String[] args) {
-        Gerente gerente1 = new Gerente("Jose", 5000, "Sistemas");
-        Gerente gerente2 = new Gerente("Santiago", 400, "Ventas");
-        System.out.println("gerente1 = " +gerente1.obtenerDetalles());
-        System.out.println("gerente2 = " +gerente2.obtenerDetalles());
+        Empleado empleado1 = new Empleado("Alberto", 1000);
+        //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
+        Gerente gerente1 = new Gerente("Jose", 5000, "Sistemas");     
+        //System.out.println("gerente1 = " +gerente1.obtenerDetalles());
+        
+        imprimir(empleado1);
+        imprimir(gerente1);
+    }
+    
+    /*Creamos metodo*/
+    public static void imprimir(Empleado empleado){
+        System.out.println("empleado = " + empleado.obtenerDetalles());
+        
     }
 }
