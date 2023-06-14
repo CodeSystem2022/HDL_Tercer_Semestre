ccb21687345be7f5c3b0236a4ee989e1f508a311
Alexis Romero <58513582+mularsoft@users.noreply.github.com>
2023-06-14 20:27:26 -0300

    Revert "Add files via upload"

--- JAVA/Leccion05/ClasesAbstractas/TestAbstractas.java
+++
@@ -1,8 +0,0 @@
-package Leccion05.ClasesAbstractas;
-
-public class TestAbstractas {
-    public static void main(String[] args) {
-        FiguraGeometrica figura = new Rectangulo("Rectangulo");
-        figura.dibujar();
-    }
-}
