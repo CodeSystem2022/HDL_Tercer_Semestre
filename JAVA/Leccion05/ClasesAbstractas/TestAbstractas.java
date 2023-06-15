57d2362efb87ca6deb5074adfa735a83e0c24d50
Milena Giménez <98053498+Ula-Rom@users.noreply.github.com>
2023-06-15 20:17:57 -0300

    Add files via upload
    Java_Semana_5

---
+++ JAVA/Leccion05/ClasesAbstractas/TestAbstractas.java
@@ -0,0 +1,8 @@
+package Leccion05.ClasesAbstractas;
+
+public class TestAbstractas {
+    public static void main(String[] args) {
+        FiguraGeometrica figura = new Rectangulo("Rectangulo");
+        figura.dibujar();
+    }
+}
