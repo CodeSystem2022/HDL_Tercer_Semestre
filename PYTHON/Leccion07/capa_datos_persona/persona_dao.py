8c942e03458039a0309aaaac69766689f19f6e3c
Alexis Romero <58513582+mularsoft@users.noreply.github.com>
2023-06-30 00:29:41 -0300

    7.3 Video de otro proyecto llamado: Pärticulas

--- PYTHON/Leccion07/capa_datos_persona/persona_dao.py
+++ PYTHON/Leccion07/capa_datos_persona/persona_dao.py
@@ -11,7 +11,6 @@ class PersonaDAO:
         Update --> Actualizar
         Delete --> Eliminar
     """
-
     _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
     _INSERTAR = 'INSERT INTO persona(nombre,apellido,email) VALUES (%s,%s,%s)'
     _ACTUALIZAR = 'UPDATE persona SET nombre=%s,apellido=%s,email=%s WHERE id_persona=%s'
