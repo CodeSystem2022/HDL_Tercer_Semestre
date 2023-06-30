6a7a5641e59a3baf6c48afa9bfa53027afd82bc8
Alexis Romero <58513582+mularsoft@users.noreply.github.com>
2023-06-30 17:53:09 -0300

    7.3 Clase object, toString, sobreescritura y Polimorfismo

--- PYTHON/Leccion07/capa_datos_persona/logger_base.py
+++ PYTHON/Leccion07/capa_datos_persona/logger_base.py
@@ -11,7 +11,6 @@ log.basicConfig(level=log.DEBUG,
                     log.FileHandler('capa_datos.log'),
                     log.StreamHandler()
                 ])
-
 if __name__ == '__main__':
     log.debug("Mensaje a nivel debug")
     log.info("Mensaje a nivel info")
